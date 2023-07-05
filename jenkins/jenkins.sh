fbcddeef4413ff284a2676dbeedec5e156b0fa4c
Dimitar Nikolov <d.nikolov7654@gmail.com>
2023-07-05 09:25:26 +0300

    automate jenkins ngrok port forwarding

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -10,17 +10,31 @@ docker compose up -d
 
 JENKINS_URL='http://localhost:8080'
 
-echo "\nAwaiting Jenkins start ...\n"
+echo "\nAwaiting Jenkins start...\n"
 until $(curl --output /dev/null --silent --head --fail $JENKINS_URL); do
     sleep 2
 done
 
 rm jenkins-cli.jar
-wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar -o /dev/null
 
 java -jar jenkins-cli.jar -s $JENKINS_URL create-job 'internship' < ./jobs/internship.xml
 
 rm jenkins-cli.jar
+
+
+# this is so the jenkins is visible to the outside world
+echo "Starting Ngrok port forward...\n"
+killall ngrok 2>/dev/null
+ngrok http 8080 > /dev/null &
+
+# so the ngrok loads
+sleep 5 
+
+tunnel_url=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "Connect your GitHub WebHook to the given link: $tunnel_url/github-webhook/\n"
+
 
 # !!! you need to add your own GitHub Credentials in the Dashboard > Manage Jenkins > System
 
